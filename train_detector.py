75d5db0f188426ba3ec2f5fa8b3c21e07b11e2ee
Sudharsan Gopalakrishnan Gopalakrishnan <sgopala2@l002.ib.bridges2.psc.edu>
2025-05-23 19:55:48 -0400

    Trained model on 5% synthetic ratio

--- train_detector.py
+++ train_detector.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 def load_model():
-    model = YOLO("yolo11m.pt")
+    model = YOLO("yolo11n.pt")
     return model
 
-def train(model, path, img_size, batch_size, epochs, device=0):
+def train(model, path, img_size, batch_size, epochs, device='cpu'):
     model.train(data=path, imgsz=img_size,batch=batch_size,epochs=epochs,device=device)
     return model
 
@@ -16,13 +16,13 @@ if __name__ == "__main__":
     img_size=640
     batch_size = 8
     epochs = 200
-    device=0
+    device='cpu'
     model = train(model, path, img_size, batch_size, epochs, device)
 
-    # Ratio 0.50
-    path = "yolo_datasets/syn0.5_size1000_90-5-5/data.yaml"
-    img_size=640
-    batch_size = 8
-    epochs = 200
-    device=0
-    model = train(model, path, img_size, batch_size, epochs, device)
+    # # Ratio 0.50
+    # path = "yolo_datasets/syn0.5_size1000_90-5-5/data.yaml"
+    # img_size=640
+    # batch_size = 8
+    # epochs = 200
+    # device='cpu'
+    # model = train(model, path, img_size, batch_size, epochs, device)
